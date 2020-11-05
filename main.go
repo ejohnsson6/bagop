91eb7b9b1a8f982fcd2aa3740d26573d6e3e5581
Swexbe <erik@erikjohnsson.se>
2020-11-05 20:07:15 +0100

    Added basic structure

--- main.go
+++ main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
 const (
-	dbLabel        = "bagop.vendor"
-	nameLabel      = "bagop.name"
-	backupLocation = "/backups"
+	ENABLE_LABEL    = "bagop.enable"
+	DB_LABEL        = "bagop.vendor"
+	NAME_LABEL      = "bagop.name"
+	BACKUP_LOCATION = "/backups"
 )
+
+func containerEnabled(container types.Container) bool {
+	labels := container.Labels
+	isEnabled := labels[ENABLE_LABEL] != "false" && labels[ENABLE_LABEL] != "" && labels[ENABLE_LABEL] != "0"
+	return isEnabled
+}
+
+func findVendor(container types.Container) string {
+	nameLabel := strings.ToLower(container.Labels[NAME_LABEL])
+	allowedVendors := []string{"mysql", "postgres"}
+	if Contains(allowedVendors, nameLabel) {
+		return nameLabel
+	}
+	return "dasd"
+}
+
+func dumpMysql(container types.Container) {
+
+}
+
+func dumpPostgres(container types.Container) {
+
+}
 
 func main() {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
@@ -26,6 +51,7 @@ func main() {
 	}
 
 	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID, container.Image)
+		fmt.Printf("%s %s\n", container.ID, container.Labels)
+		fmt.Println(containerEnabled(container))
 	}
 }
