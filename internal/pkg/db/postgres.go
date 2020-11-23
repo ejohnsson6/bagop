c250fe08de01b73e073a4a51c5da5452cda1e119
Erik Johnsson <erik@erikjohnsson.se>
2020-11-23 23:09:31 +0100

    Default username&db for postgres

--- internal/pkg/db/postgres.go
+++ internal/pkg/db/postgres.go
@@ -8,7 +8,15 @@ import (
 func DumpPostgresCmd(env []string) []string {
 
 	username := findEnvVar(env, "POSTGRES_USER")
+	if username == "" {
+		// Default username (https://hub.docker.com/_/postgres)
+		username = "postgres"
+	}
 	db := findEnvVar(env, "POSTGRES_DB")
+	if db == "" {
+		// Default DB name will be set to the value of $POSTGRES_USER (https://hub.docker.com/_/postgres)
+		db = username
+	}
 
 	command := []string{"pg_dump", fmt.Sprintf("--username=%s", username), db}
 
