b0f0990fb283f9cb7ac2d460181534360d002af4
Erik Johnsson <erik@swex.be>
2021-03-02 21:24:46 +0100

    Implemented support for long term backups every n run

--- run_and_wait.sh
+++ run_and_wait.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 while true
 do
-    bagop -b -ttl=$TTL
-    bagop -c
-    sleep $SLEEP
+    for (( c=1; c<$LT_ITER; c++ ))
+    do
+        bagop -b -ttl=$TTL
+        bagop -c
+        sleep $SLEEP
+    done
+    # Run Long-term backup once every $LT_ITER if exported
+    if [ -n "$LT_ITER" ]; then
+        bagop -b -ttl=$LT_TTL
+        bagop -c
+        sleep $SLEEP
+    fi
 done
