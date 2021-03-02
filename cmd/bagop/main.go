cb1f88130489b4e61bb5ae43c3ed603dae9215a4
Erik Johnsson <erik@swex.be>
2021-03-02 20:21:58 +0100

    Added verbose mode

--- cmd/bagop/main.go
+++ cmd/bagop/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	l "github.com/swexbe/bagop/internal/pkg/logging"
 	"github.com/swexbe/bagop/internal/pkg/utility"
 )
@@ -19,10 +20,16 @@ func main() {
 	godotenv.Load()
 	clean := flag.Bool("c", false, "Clean: Remove archives which have expired")
 	backup := flag.Bool("b", false, "Backup: Make a backup and push it to Glacier")
-	version := flag.Bool("v", false, "Version: Display version")
+	version := flag.Bool("version", false, "Version: Display version")
+	verbose := flag.Bool("v", false, "Verbose: Display debug information")
 	ttl := flag.String("ttl", "", "Time to Live: Number of days until archives will be deleted")
 
 	flag.Parse()
+
+	if *verbose {
+		l.Logger.SetLevel(logrus.DebugLevel)
+		l.Logger.Debugf("Running in verbose mode")
+	}
 	if *clean {
 		cleanBackups()
 	} else if *backup {
