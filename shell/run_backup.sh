201a5a9326b2121cae9b8edbd2e5fd579f0162d2
Erik Johnsson <erik@swex.be>
2021-03-09 16:04:27 +0100

    Use lock to only allow one backup at a time

--- shell/run_backup.sh
+++ shell/run_backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo "****RUNNING REGULAR BACKUP****"
-bagop -b -force-color=${BAGOP_COLOR:-true} -v=${BAGOP_VERBOSE:-false} -ttl=$BAGOP_TTL
-echo "****RUNNING CLEAN****"
-bagop -c -force-color=${BAGOP_COLOR:-true} -v=${BAGOP_VERBOSE:-false}
+# Makes sure we exit if flock fails.
+set -e
+(
+    echo "****WAITING FOR LOCK****"
+    # Wait for lock on /var/lock/.bagop (fd 200) for 10000 seconds
+    flock -x 200
+    echo "****RUNNING REGULAR BACKUP****"
+    # Do sbackup
+    bagop -b -force-color=${BAGOP_COLOR:-true} -v=${BAGOP_VERBOSE:-false} -ttl=$BAGOP_TTL
+    echo "****RUNNING CLEAN****"
+    bagop -c -force-color=${BAGOP_COLOR:-true} -v=${BAGOP_VERBOSE:-false}
+) 200>/var/lock/.bagop
