fa5825757f092f6195a88526e64906dfb52be1af
Erik Johnsson <erik@swex.be>
2021-03-09 17:33:01 +0100

    Log to file instead of fifo

--- shell/cron.sh
+++ shell/cron.sh
@@ -3,20 +3,19 @@
 printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export (BAGOP|AWS)" >/root/project_env.sh
 
 # Create new CRONTAB based on $CRON and $LT_CRON, redirect output to stdout/stderr
-echo "${CRON?Error CRON env not defined} . /root/project_env.sh; /home/root/run_backup.sh > /tmp/stdout 2> /tmp/stderr" >/tmp/bagop-cron
+echo "${CRON?Error CRON env not defined} . /root/project_env.sh; /home/root/run_backup.sh >> /var/bagop/log 2>&1" >/tmp/bagop-cron
 if [ -n "$LT_CRON" ]; then
-  echo "$LT_CRON . /root/project_env.sh; /home/root/run_lt_backup.sh > /tmp/stdout 2> /tmp/stderr" >>/tmp/bagop-cron
+  echo "$LT_CRON . /root/project_env.sh; /home/root/run_lt_backup.sh >> /var/bagop/log 2>&1" >>/tmp/bagop-cron
 fi
 
 # Replace root crontab
 cat /tmp/bagop-cron >/etc/crontabs/root
 
-# Create custom stdout and stderr named pipes
-mkfifo /tmp/stdout /tmp/stderr
-chmod 0666 /tmp/stdout /tmp/stderr
+# Create logfile
+touch /var/bagop/log
+chmod 0666 /var/bagop/log
 
-crond
-# Have the main Docker process tail the files to produce stdout and stderr
-# for the main process that Docker will actually show in docker logs.
-tail -f /tmp/stdout &
-tail -f /tmp/stderr >&2
+crond -b
+
+# Tail logfile
+tail -n 0 -F /var/bagop/log
